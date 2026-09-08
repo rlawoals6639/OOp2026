02dd226c72e1f6a85eb532ea95bc90f0121dafda
rlawoals6639 <rlawoals6639@gmail.com>
2026-09-08 20:12:16 +0900

    Create fibonacci.java

---
+++ 1to4/fibonacci.java
@@ -0,0 +1,17 @@
+public class fibonacci {
+
+	public static void main(String[] args) {
+		
+		int i;
+        int j = 1;
+        int k = 1;
+
+        for(i=0; i<=10; i++) {
+            System.out.print(j + " " + k + " ");
+
+            j+=k;
+            k+=j;
+
+        }
+	}
+}
