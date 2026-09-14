dc46ff840ee638138523838c2dd10dbdf8d11e72
rlawoals6639 <rlawoals6639@gmail.com>
2026-09-14 18:58:07 +0900

    Create pi.java

---
+++ 5to8/pi.java
@@ -0,0 +1,22 @@
+package object_coding;
+
+public class pi {
+
+	public static void main(String[] args) {
+        int n = 15;
+        double sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            double x = 1.0 / ((2 * i + 1) * Math.pow(3, i));
+            if (i % 2 == 0) {
+                sum += x;
+            } else {
+                sum -= x;
+            }
+        }
+
+        double pi = Math.sqrt(12) * sum;
+
+        System.out.printf("%.15f%n", pi);
+    }
+}
