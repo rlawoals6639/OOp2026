01009af3c9312bec960123d59f9e7c43921ce760
rlawoals6639 <rlawoals6639@gmail.com>
2026-09-14 19:11:57 +0900

    Update golden_ratio.java

--- 1to4/golden_ratio.java
+++ 1to4/golden_ratio.java
@@ -1,17 +1,16 @@
 public class golden_ratio {
+	
+    public static void main(String[] args) {
+    	int n = 20;
+        long a = 1;
+        long b = 1;
 
-	public static void main(String[] args) {
-		
-		int i;
-        int j = 1;
-        int k = 1;
-
-        for(i=0; i<=10; i++) {
-            System.out.print(j + " " + k + " ");
-
-            j+=k;
-            k+=j;
-
+        for (int i = 1; i < n; i++) {
+            long next = a + b;
+            double ratio = (double) next / b;
+            System.out.println(next + "/" + b + "=" + ratio);
+            a = b;
+            b = next;
         }
-	}
+    }
 }
