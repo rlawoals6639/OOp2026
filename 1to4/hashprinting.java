72386101532247f41fab6445b2736bf0d0aec2e7
rlawoals6639 <rlawoals6639@gmail.com>
2026-09-14 18:59:39 +0900

    Update hashprinting.java
    Added multiple patterns of hash printing to the main method.

--- 1to4/hashprinting.java
+++ 1to4/hashprinting.java
@@ -1,1 +1,44 @@
+public class hash_printing{
+  public static void main(String []args){
+    int i, j, k;
+    for(i=0; i<10; i++) {
+      for(j=0; j<10-i; j++) {
+        System.out.print("#");
+      }
+      System.out.println("");
+    }
 
+    //============================
+    
+    for(i=0; i<10; i++) {
+      for(j=0; j<=i; j++) {
+        System.out.print("#");
+      }
+      System.out.println("");
+    }
+
+    //============================
+
+    for(i=0; i<10; i++) {
+      for(j=0; j<i; j++) {
+        System.out.print(" ");
+      }
+      for(k=0; k<=9-i; k++) {
+      System.out.print("#");
+      }
+      System.out.println("");
+    }
+
+    //============================
+
+    for(i=0; i<10; i++) {
+      for(j=0; j<9-i; j++) {
+        System.out.print(" ");
+      }
+      for(k=0; k<=i; k++) {
+      System.out.print("#");
+      }
+      System.out.println("");
+    }
+  }
+}
